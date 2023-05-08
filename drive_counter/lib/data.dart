2d6e25534341cd61f960e2e7ca7bcb3b253cef89
Tim Hämisch <haemtim@gmail.com>
2023-05-08 11:20:43 +0200

    app: added stats page

--- drive_counter/lib/data.dart
+++ drive_counter/lib/data.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vibrate/flutter_vibrate.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const Widget divider = SizedBox(height: 10);
 
@@ -8,67 +9,64 @@ const double narrowScreenWidthThreshold = 700;
 class history extends StatelessWidget {
   const history({super.key});
 
-  void handleExportPressed() {}
-
-  void handleImportPressed() {}
-
-  void vibrateWarning() {
-    Vibrate.feedback(FeedbackType.warning);
-  }
-
-  void vibrateSuccess() {
-    Vibrate.feedback(FeedbackType.success);
-  }
-
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     final double screenWidth = screenSize.width;
     final double screenHeight = screenSize.height - 200;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: screenHeight / 10,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                    width: screenWidth,
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        handleExportPressed();
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text('EXPORT DATA '),
-                        ],
-                      ),
-                    )),
-                SizedBox(
-                  width: screenWidth,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      handleImportPressed();
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('IMPORT DATA '),
-                      ],
-                    ),
-                  ),
-                ),
-              ]),
+
+    String overallCustomers;
+    String overallTime;
+    String avgTimePerCustomer;
+    String avgQueueTime;
+    String minQueueTime;
+    String maxQueueTime;
+    String avgOrderTime;
+    String minOrderTime;
+    String maxOrderTime;
+    String avgPickupTime;
+    String minPickupTime;
+    String maxPickupTime;
+
+    return SizedBox(
+      width: screenWidth,
+      height: screenHeight,
+      child: Column(
+        children: [
+          StatCard(title: '5s', subtitle: 'overall customers served'),
+          StatCard(title: '5s', subtitle: 'overall time'),
+          StatCard(title: '5s', subtitle: 'avg. time per customer'),
+          StatCard(title: '5s', subtitle: 'avg. queue time'),
+          StatCard(title: '5s', subtitle: 'min. queue time'),
+          StatCard(title: '5s', subtitle: 'max. queue time'),
+          StatCard(title: '5s', subtitle: 'avg. order time'),
+          StatCard(title: '5s', subtitle: 'min. order time'),
+          StatCard(title: '5s', subtitle: 'max. order time'),
+          StatCard(title: '5s', subtitle: 'avg. pickup time'),
+          StatCard(title: '5s', subtitle: 'min. pickup time'),
+          StatCard(title: '5s', subtitle: 'max. pickup time'),
+        ],
+      ),
+    );
+  }
+}
+
+class StatCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  StatCard({required this.title, required this.subtitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
         ),
-      ],
+        subtitle: Text(subtitle),
+      ),
     );
   }
 }
