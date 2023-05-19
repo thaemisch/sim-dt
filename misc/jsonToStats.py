b33ac443459f78bc956eeb4c51072aeaeb157122
Tim Hämisch <haemtim@gmail.com>
2023-05-19 19:58:48 +0200

    stats

--- misc/jsonToStats.py
+++ misc/jsonToStats.py
@@ -1,13 +1,14 @@
 import json
+from datetime import datetime, timedelta
 
 # Read the JSON file
-with open('lists.json') as f:
+with open('test.json') as f:
     json_data = json.load(f)
 
 # Convert the JSON data to lists of datetimes
 lists = []
 for json_list in json_data:
-    dt_list = [datetime.fromisoformat(dt_str) for dt_str in json_list]
+    dt_list = [datetime.strptime(dt_str, "%H:%M:%S") for dt_str in json_list]
     lists.append(dt_list)
 
 # Use the lists in your Python program
