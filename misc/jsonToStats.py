d4117346e0ddb236ce9d138f91df99e2cb5a51de
Tim Hämisch <haemtim@gmail.com>
2023-05-19 21:11:45 +0200

    auswertung

--- misc/jsonToStats.py
+++ misc/jsonToStats.py
@@ -11,18 +11,71 @@ for json_list in json_data:
     dt_list = [datetime.strptime(dt_str, "%H:%M:%S") for dt_str in json_list]
     lists.append(dt_list)
 
-# Use the lists in your Python program
-#for lst in lists:
-#    print(lst)
+queue = lists[0]
+order = lists[1]
+pickup_queue = lists[2]
+pickup = lists[3]
+exit = lists[4]
 
-def calcQueue():
-    print("Calculating queue...")
+queue_deltas = []
+order_deltas = []
+pickup_queue_deltas = []
+pickup_deltas = []
 
-def calcOrder():
-    print("Calculating order...")
+sum_queue_deltas = timedelta(0)
+sum_order_deltas = timedelta(0)
+sum_pickup_queue_deltas = timedelta(0)
+sum_pickup_deltas = timedelta(0)
 
-def calcPickupQueue():
-    print("Calculating pickup queue...")
+for i in range(len(queue)):
+    queue_deltas.append(order[i] - queue[i])
+max_queue_delta = max(queue_deltas)
+min_queue_delta = min(queue_deltas)
+for i in range(len(queue_deltas)):
+    sum_queue_deltas += queue_deltas[i]
+avg_queue_delta = sum_queue_deltas / len(queue_deltas)
 
-def calcPickup():
-    print("Calculating pickup...")
+for i in range(len(order)):
+    order_deltas.append(pickup_queue[i] - order[i])
+max_order_delta = max(order_deltas)
+min_order_delta = min(order_deltas)
+for i in range(len(order_deltas)):
+    sum_order_deltas += order_deltas[i]
+avg_order_delta = sum_order_deltas / len(order_deltas)
+
+for i in range(len(pickup_queue)):
+    pickup_queue_deltas.append(pickup[i] - pickup_queue[i])
+max_pickup_queue_delta = max(pickup_queue_deltas)
+min_pickup_queue_delta = min(pickup_queue_deltas)
+for i in range(len(pickup_queue_deltas)):
+    sum_pickup_queue_deltas += pickup_queue_deltas[i]
+avg_pickup_queue_delta = sum_pickup_queue_deltas / len(pickup_queue_deltas)
+
+for i in range(len(pickup)):
+    pickup_deltas.append(exit[i] - pickup[i])
+max_pickup_delta = max(pickup_deltas)
+min_pickup_delta = min(pickup_deltas)
+for i in range(len(pickup_deltas)):
+    sum_pickup_deltas += pickup_deltas[i]
+avg_pickup_delta = sum_pickup_deltas / len(pickup_deltas)
+
+print("----------------------------------------")
+print("Queue")
+print("Max: " + str(max_queue_delta))
+print("Min: " + str(min_queue_delta))
+print("Avg: " + str(avg_queue_delta))
+print("----------------------------------------")
+print("Order")
+print("Max: " + str(max_order_delta))
+print("Min: " + str(min_order_delta))
+print("Avg: " + str(avg_order_delta))
+print("----------------------------------------")
+print("Pickup Queue")
+print("Max: " + str(max_pickup_queue_delta))
+print("Min: " + str(min_pickup_queue_delta))
+print("Avg: " + str(avg_pickup_queue_delta))
+print("----------------------------------------")
+print("Pickup")
+print("Max: " + str(max_pickup_delta))
+print("Min: " + str(min_pickup_delta))
+print("Avg: " + str(avg_pickup_delta))
