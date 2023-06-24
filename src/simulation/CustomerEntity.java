e88ad0df7a7dc7b1d5e3dafed76f9524171d38b9
Tim Hämisch <haemtim@gmail.com>
2023-06-24 13:38:14 +0200

    added basic enities

---
+++ src/simulation/CustomerEntity.java
@@ -0,0 +1,10 @@
+package simulation;
+
+import desmoj.core.simulator.*;
+
+public class CustomerEntity extends Entity {
+    public CustomerEntity(Model owner, String name, boolean showInTrace) {
+        super(owner, name, showInTrace);
+    }
+
+}
