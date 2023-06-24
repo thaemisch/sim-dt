08ecc91ff8422157855d5717c6f89fa6505e7454
Tim Hämisch <haemtim@gmail.com>
2023-06-24 21:10:20 +0200

    updated stats and included markdown to display stats

--- data/jsonToStats.py
+++ data/jsonToStats.py
@@ -1,5 +1,6 @@
 import json
 from datetime import datetime, timedelta
+import markdown
 
 filename = input("Enter the filename: ")
 
@@ -20,68 +21,78 @@ pickup = lists[3]
 exit = lists[4]
 
 queue_deltas = []
+queue_deltas_sideways = []
 order_deltas = []
 pickup_queue_deltas = []
 pickup_deltas = []
 
-sum_queue_deltas = timedelta(0)
-sum_order_deltas = timedelta(0)
-sum_pickup_queue_deltas = timedelta(0)
-sum_pickup_deltas = timedelta(0)
+# Calculate min/max of new arrivals
+for i in range(len(queue)-1):
+    queue_deltas_sideways.append(queue[i+1] - queue[i])
+max_queue_delta_sideways = max(queue_deltas_sideways)
+min_queue_delta_sideways = min(queue_deltas_sideways)
 
-# Calculate Queue
+# Calculate min/max of each station
+# Queue
 for i in range(len(queue)):
     queue_deltas.append(order[i] - queue[i])
 max_queue_delta = max(queue_deltas)
 min_queue_delta = min(queue_deltas)
-for i in range(len(queue_deltas)):
-    sum_queue_deltas += queue_deltas[i]
-avg_queue_delta = sum_queue_deltas / len(queue_deltas)
 
-# Calculate Order
+# Order
 for i in range(len(order)):
     order_deltas.append(pickup_queue[i] - order[i])
 max_order_delta = max(order_deltas)
 min_order_delta = min(order_deltas)
-for i in range(len(order_deltas)):
-    sum_order_deltas += order_deltas[i]
-avg_order_delta = sum_order_deltas / len(order_deltas)
 
-# Calculate Pickup Queue
+# Pickup Queue
 for i in range(len(pickup_queue)):
     pickup_queue_deltas.append(pickup[i] - pickup_queue[i])
 max_pickup_queue_delta = max(pickup_queue_deltas)
 min_pickup_queue_delta = min(pickup_queue_deltas)
-for i in range(len(pickup_queue_deltas)):
-    sum_pickup_queue_deltas += pickup_queue_deltas[i]
-avg_pickup_queue_delta = sum_pickup_queue_deltas / len(pickup_queue_deltas)
 
-# Calculate Pickup
+# Pickup
 for i in range(len(pickup)):
     pickup_deltas.append(exit[i] - pickup[i])
 max_pickup_delta = max(pickup_deltas)
 min_pickup_delta = min(pickup_deltas)
-for i in range(len(pickup_deltas)):
-    sum_pickup_deltas += pickup_deltas[i]
-avg_pickup_delta = sum_pickup_deltas / len(pickup_deltas)
 
-print("----------------------------------------")
-print("Queue")
-print("Max: " + str(max_queue_delta))
+# Print results
+print("New arraival:")
+print("Min: " + str(min_queue_delta_sideways))
+print("Max: " + str(max_queue_delta_sideways))
+print("--------------------")
+print("Queue:")
 print("Min: " + str(min_queue_delta))
-print("Avg: " + str(avg_queue_delta))
-print("----------------------------------------")
-print("Order")
-print("Max: " + str(max_order_delta))
+print("Max: " + str(max_queue_delta))
+print("--------------------")
+print("Order:")
 print("Min: " + str(min_order_delta))
-print("Avg: " + str(avg_order_delta))
-print("----------------------------------------")
-print("Pickup Queue")
-print("Max: " + str(max_pickup_queue_delta))
+print("Max: " + str(max_order_delta))
+print("--------------------")
+print("Pickup Queue:")
 print("Min: " + str(min_pickup_queue_delta))
-print("Avg: " + str(avg_pickup_queue_delta))
-print("----------------------------------------")
-print("Pickup")
-print("Max: " + str(max_pickup_delta))
+print("Max: " + str(max_pickup_queue_delta))
+print("--------------------")
+print("Pickup:")
 print("Min: " + str(min_pickup_delta))
-print("Avg: " + str(avg_pickup_delta))
+print("Max: " + str(max_pickup_delta))
+
+# Create a markdown string with the variables and descriptions as a table
+markdown_string = f"""| Legend | Value |
+| --- | --- |
+| New arrival: Min | {min_queue_delta_sideways} |
+| New arrival: Max | {max_queue_delta_sideways} |
+| Queue: Min | {min_queue_delta} |
+| Queue: Max | {max_queue_delta} |
+| Order: Min | {min_order_delta} |
+| Order: Max | {max_order_delta} |
+| Pickup Queue: Min | {min_pickup_queue_delta} |
+| Pickup Queue: Max | {max_pickup_queue_delta} |
+| Pickup: Min | {min_pickup_delta} |
+| Pickup: Max | {max_pickup_delta} |
+"""
+
+# Write the markdown string to a file
+with open("output.md", "w") as f:
+    f.write(markdown.markdown(markdown_string))
---
+++ data/output.md
@@ -0,0 +1,12 @@
+<p>| Legend | Value |
+| --- | --- |
+| New arrival: Min | 0:00:05 |
+| New arrival: Max | 0:04:01 |
+| Queue: Min | 0:00:00 |
+| Queue: Max | 0:01:04 |
+| Order: Min | 0:00:18 |
+| Order: Max | 0:01:23 |
+| Pickup Queue: Min | 0:00:00 |
+| Pickup Queue: Max | 0:03:16 |
+| Pickup: Min | 0:00:08 |
+| Pickup: Max | 0:03:20 |</p>
