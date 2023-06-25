127f10e0553b93d82860c29c6b1fe4cce797f0f5
Tim Hämisch <haemtim@gmail.com>
2023-06-25 14:00:09 +0200

    added pickup once again

---
+++ src/simulation/PickupExitEvent.java
@@ -0,0 +1,27 @@
+package simulation;
+
+import desmoj.core.simulator.*;
+
+public class PickupExitEvent extends Event<CustomerEntity> {
+
+    private DT_model myModel;
+
+    public PickupExitEvent(Model owner, String name, boolean showInTrace) {
+        super(owner, name, showInTrace);
+        myModel = (DT_model) owner;
+    }
+
+    public void eventRoutine(CustomerEntity customer) {
+        if (!myModel.pickupQueue.isEmpty()) {
+            CustomerEntity nextCustomer = myModel.pickupQueue.first();
+            myModel.pickupQueue.remove(nextCustomer);
+
+            PickupExitEvent pickupExit = new PickupExitEvent(myModel, "Pickup Exit", true);
+            pickupExit.schedule(nextCustomer, new TimeSpan(myModel.getPickupTime()));
+        } else {
+            OrderEntity order = myModel.busyPickupWindow.first();
+            myModel.busyPickupWindow.remove(order);
+            myModel.freePickupWindow.insert(order);
+        }
+    }
+}
