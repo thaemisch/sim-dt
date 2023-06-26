a26f60b0f9864ea817167d701e64dde15e0296d7
Tim Hämisch <haemtim@gmail.com>
2023-06-26 07:00:41 +0200

    added stoß- und nebenzeit

--- src/simulation/DT_model.java
+++ src/simulation/DT_model.java
@@ -7,8 +7,15 @@ public class DT_model extends Model {
     // Variables
     static double startTime = 0.0;
     static double endTime = 240.0;
-
+    static Boolean stoßzeit = false;
+    static Boolean nebenzeit = false;
     public static Boolean quiet = false;
+
+    static double arrivalTimeDiff = 0.0;
+    static double orderTimeStartDiff = 0.0;
+    static double orderTimeEndDiff = 0.0;
+    static double pickupTimeStartDiff = 0.0;
+    static double pickupTimeEndDiff = 0.0;
 
     public String description() {
         return "DT_model (Ereignisorientiert):" +
@@ -59,7 +66,13 @@ public class DT_model extends Model {
     }
 
     public void init() {
-        defaultInit();
+        if (stoßzeit){
+            customInit(1.04+arrivalTimeDiff, 0.1+orderTimeStartDiff, 2.17+orderTimeEndDiff, 0.6+pickupTimeStartDiff, 4.52+pickupTimeEndDiff);
+        } else if (nebenzeit){
+            customInit(1.2+arrivalTimeDiff, 0.18+orderTimeStartDiff, 1.23+orderTimeEndDiff, 0.08+pickupTimeStartDiff, 3.2+pickupTimeEndDiff);
+        } else {
+            defaultInit();
+        }
     }
 
     public void defaultInit() {
@@ -84,6 +97,28 @@ public class DT_model extends Model {
         busyPickupWindow = new Queue<PickupEntity>(this, "BusyPickupWindow", true, true);
     }
 
+    public void customInit(Double arrivalTime, Double orderTimeStart, Double orderTimeEnd, Double pickupTimeStart, Double pickupTimeEnd) {
+        // Order
+        customerArrivalTime = new ContDistExponential(this, "CustomerArrivalTime", arrivalTime, true, false);
+        customerArrivalTime.setNonNegative(true);
+        orderTime = new ContDistUniform(this, "OrderTime", orderTimeStart, orderTimeEnd, true, false);
+        orderQueue = new Queue<CustomerEntity>(this, "OrderQueue", true, true);
+        freeOrderWindow = new Queue<OrderEntity>(this, "FreeOrderWindow", true, true);
+        OrderEntity order;
+        order = new OrderEntity(this, "Order", true);
+        freeOrderWindow.insert(order);
+        busyOrderWindow = new Queue<OrderEntity>(this, "BusyOrderWindow", true, true);
+
+        // Pickup
+        pickupTime = new ContDistUniform(this, "PickupTime", pickupTimeStart, pickupTimeEnd, true, false);
+        pickupQueue = new Queue<CustomerEntity>(this, "PickupQueue", true, true);
+        freePickupWindow = new Queue<PickupEntity>(this, "FreePickupWindow", true, true);
+        PickupEntity pickup;
+        pickup = new PickupEntity(this, "Pickup", true);
+        freePickupWindow.insert(pickup);
+        busyPickupWindow = new Queue<PickupEntity>(this, "BusyPickupWindow", true, true);
+    }
+
     public static void main(java.lang.String[] args){
         // Parse CLI arguments
         if (args.length > 0) {
@@ -98,6 +133,8 @@ public class DT_model extends Model {
                         endTime = Double.parseDouble(args[i + 1]);
                         i++;
                     }
+                    case "--stoßzeit" -> stoßzeit = true;
+                    case "--nebenzeit" -> nebenzeit = true;
                     case "--quiet", "-q" -> quiet = true;
                     case "--help", "-h" -> {
                         System.out.println("Options:");
