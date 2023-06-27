3d1091515a481516896105e503c28563e87efa90
Tim Hämisch <haemtim@gmail.com>
2023-06-27 15:06:59 +0200

    fixed wrong queu limits

--- src/simulation/CustomerArrivalPickupEvent.java
+++ src/simulation/CustomerArrivalPickupEvent.java
@@ -5,6 +5,7 @@ import desmoj.core.simulator.*;
 public class CustomerArrivalPickupEvent extends Event<CustomerEntity>{
 
     private DT_model myModel;
+    private Boolean insertedCustomer = false;
 
     public CustomerArrivalPickupEvent(Model owner, String name, boolean showInTrace) {
         super(owner, name, showInTrace);
@@ -12,12 +13,21 @@ public class CustomerArrivalPickupEvent extends Event<CustomerEntity>{
     }
 
     public void eventRoutine(CustomerEntity customer) {
-        myModel.pickupQueue.insert(customer);
-        customer.setPickupQueueEntry(myModel.presentTime().getTimeAsDouble());
-        data.silentScreamer(myModel.presentTime().getTimeAsDouble() + " | Pickup Queue: Customer" + customer.getName() + " arrived");
-        data.chronoLogger("pq", myModel.presentTime().getTimeAsDouble());
-        if (!myModel.freePickupWindow.isEmpty()) {
-
+        if (myModel.getPickupQueueLimit() == 0){
+            myModel.pickupQueue.insert(customer);
+            insertedCustomer = true;
+        } else if (myModel.getPickupQueueLimit() > 0 && myModel.pickupQueue.length() < myModel.getPickupQueueLimit()) {
+            myModel.pickupQueue.insert(customer);
+            insertedCustomer = true;
+        } else {
+            System.out.println("Pickup Queue | Customer rejected");
+        }
+        if (insertedCustomer) {
+            customer.setPickupQueueEntry(myModel.presentTime().getTimeAsDouble());
+            data.silentScreamer(myModel.presentTime().getTimeAsDouble() + " | Pickup Queue: Customer" + customer.getName() + " arrived");
+            data.chronoLogger("pq", myModel.presentTime().getTimeAsDouble());
+        }
+        if (insertedCustomer && !myModel.freePickupWindow.isEmpty()) {
             data.silentScreamer(myModel.presentTime().getTimeAsDouble() + " | Pickup Queue: Customer" + customer.getName() + " left");
             myModel.pickupQueue.remove(customer);
             customer.setPickupQueueExit(myModel.presentTime().getTimeAsDouble());
